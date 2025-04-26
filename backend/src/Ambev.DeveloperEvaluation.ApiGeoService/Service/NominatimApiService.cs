2424b776309a679ea6bc797c795dcd9a989f0c12
Thiago Salles <thiagobasalles@gmail.com>
2025-04-25 21:52:00 -0300

    fix ioc

---
+++ backend/src/Ambev.DeveloperEvaluation.ApiGeoService/Service/NominatimApiService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ambev.DeveloperEvaluation.Domain.Dtos;
+using Ambev.DeveloperEvaluation.Domain.Services;
+using Nominatim.API.Address;
+using Nominatim.API.Geocoders;
+using Nominatim.API.Interfaces;
+using Nominatim.API.Models;
+
+namespace Ambev.DeveloperEvaluation.ApiGeoService.Services
+{
+    public class NominatimApiService : IGeoApiService
+    {
+        private readonly IAddressSearcher _addressSearcher;
+        private readonly IForwardGeocoder _forwardGeocoder;
+        public NominatimApiService(IAddressSearcher  addressSearcher, IForwardGeocoder forwardGeocoder)
+        {
+            _addressSearcher = addressSearcher;
+            _forwardGeocoder = forwardGeocoder;
+        }
+
+        public async Task<GeoApiDto> GetByAddress(string address)
+        {
+            var request = new ForwardGeocodeRequest
+            {
+                queryString = "Av Tenente Coronel Muniz de Aragão 800",
+            };
+
+            var response = (await _forwardGeocoder.Geocode(request))[0];
+
+            var dto = new GeoApiDto
+            {
+                Lat = response.Latitude,
+                Lon = response.Longitude
+            };
+
+            return dto;
+        }
+    }
+}
--- backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/ApplicationModuleInitializer.cs
+++ backend/src/Ambev.DeveloperEvaluation.IoC/ModuleInitializers/ApplicationModuleInitializer.cs
@@ -1,6 +1,14 @@
-﻿using Ambev.DeveloperEvaluation.Common.Security;
+﻿using Ambev.DeveloperEvaluation.Cache.Services;
+using Ambev.DeveloperEvaluation.Common.Security;
+using Ambev.DeveloperEvaluation.Domain.Caches;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using Ambev.DeveloperEvaluation.Domain.Services;
+using Ambev.DeveloperEvaluation.ORM.Repositories;
+using Ambev.DeveloperEvaluation.ORM;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Ambev.DeveloperEvaluation.ApiGeoService.Services;
 
 namespace Ambev.DeveloperEvaluation.IoC.ModuleInitializers;
 
@@ -9,5 +17,15 @@ public class ApplicationModuleInitializer : IModuleInitializer
     public void Initialize(WebApplicationBuilder builder)
     {
         builder.Services.AddSingleton<IPasswordHasher, BCryptPasswordHasher>();
+
+        builder.Services.AddScoped<DbContext>(provider => provider.GetRequiredService<DefaultContext>());
+
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+
+        builder.Services.AddScoped<ICartCacheService, CartCacheService>();
+
+        builder.Services.AddScoped<IGeoApiService, NominatimApiService>();
     }
 }
